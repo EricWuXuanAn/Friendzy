f69bb1fbd21c75c0330e6673928c77eed5d0eaf7
jun <caoweijun4451@gmail.com>
2024-12-19 22:42:33 +0800

    Screen加上ScrollableTabRow,VM新增測試用資料

--- app/src/main/java/com/example/tip102group01friendzy/ui/feature/companion/Companion.kt
+++ app/src/main/java/com/example/tip102group01friendzy/ui/feature/companion/Companion.kt
@@ -3,27 +3,23 @@ package com.example.tip102group01friendzy.ui.feature.companion
 import com.example.tip102group01friendzy.R
 
 
-//依功能分VM 陪伴者及推薦項目
-class Companion(
-    var companionNO :String = "",//陪伴者(會員編號)
-    var companionCity :String = "",//陪伴者所在城市
-    var serviceId :String = "",//服務單號(流水號)
-    var service: String ="",//服務項目
-    var serviceImg :Int = R.drawable.friendzy, //服務項目照片
+data class Companion(
+//    var companionNO :String = "",//陪伴者(會員編號)
+//    var companionCity :String = "",//陪伴者所在城市
     var memberNo :String = "",//刊登者(會員編號)
     var memberName :String = "",//刊登者名稱
-    var memberCity :String = "",//刊登者城市
+//    var memberCity :String = "",//刊登者城市
     var memberImg :Int = R.drawable.friendzy, //刊登者頭像
-
-//    var serviceSkill :List<SkillItem> = listOf(),//需要專長
-//    var companionSkill :List<SkillItem> = listOf(),//陪伴者專長
-
+//    var serviceId :String = "",//服務單號(流水號)
+    var service: String ="",//服務項目
+//    var serviceImg :Int = R.drawable.friendzy, //服務項目照片
 ) {
-}
+    override fun equals(other: Any?): Boolean {
+        return this.memberNo == (other as Companion).memberNo
+    }
 
-//取專長
-class SkillItem(
-
-){
+    override fun hashCode(): Int {
+        return memberNo.hashCode()
+    }
 
 }
--- app/src/main/java/com/example/tip102group01friendzy/ui/feature/companion/CompanionScreen.kt
+++ app/src/main/java/com/example/tip102group01friendzy/ui/feature/companion/CompanionScreen.kt
@@ -1,18 +1,35 @@
 package com.example.tip102group01friendzy.ui.feature.companion
 
+import android.icu.text.Transliterator.Position
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.TabRow
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -21,30 +38,48 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.tip102group01friendzy.R
+import com.example.tip102group01friendzy.Screen
 
-
-@Composable
-fun CompanionRoute(
-    navController: NavHostController = rememberNavController(),
-    companionVM: CompanionVM
-){
-    CompanionScreen(
-
-    )
-}
-
+//tab選項內容格式
+class Tabs(var name: String = "",var btIcon: Int = R.drawable.order_manage)
 
 @Composable
 fun CompanionScreen(
-    items: List<Any> = listOf()
-
+    navController: NavHostController = rememberNavController(),
+    companionVM: CompanionVM,
     ){
-    var inputText by remember { mutableStateOf("") }
+    var inputText by remember { mutableStateOf("") }//搜尋內容
     var tabIndex by remember { mutableIntStateOf(0) }
+    var testiten by remember { mutableStateOf("") }//測試用
+    var tabs :List<Tabs> =listOf(//tab選項內容
+        Tabs("訂單管理",R.drawable.order_manage),
+        Tabs("可約時間",R.drawable.date_range),
+        Tabs("申請項目",R.drawable.check_list)
+    )
+    //tab選項點擊功能
+    when(tabIndex){
+        0 ->{//訂單管理
+            testiten = "123"
+        }
+        1 ->{//可預約時間
+            testiten = "456"
+        }
+        2 ->{//申請項目
+            testiten = "asd"
+        }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -72,17 +107,56 @@ fun CompanionScreen(
             shape = RoundedCornerShape(15.dp),
         )
         HorizontalDivider()
-        TabRow(
+        ScrollableTabRow(
             selectedTabIndex = tabIndex,
-
-        ) { }
+            modifier = Modifier.fillMaxWidth(),
+            //目前選擇選項的底線
+            indicator = {tabPositions ->
+                TabRowDefaults.SecondaryIndicator(
+                    Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
+                    color = Color.White
+                )
+            }, //分隔線屬性
+            divider = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(5.dp)
+                )
+            },
+        ) {
+            tabs.forEachIndexed{index,tabList ->
+                Tab(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(6.dp),
+                    selected = index == tabIndex,
+                    onClick = {tabIndex = index},
+                ){//選項樣式
+                    Icon(
+                        painter = painterResource(tabList.btIcon),
+                        contentDescription = tabs[index].name,
+                    )
+                    Text(text = tabs[index].name)
+                    }
+                }
+            }
+        HorizontalDivider()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text("推薦項目", fontSize = 28.sp)
+            Text(text = testiten)
+        }
     }
 }
+
 
 
 
 @Composable
 @Preview(showBackground = true)
 fun PreviewCompanionScreen(){
-    CompanionScreen()
+    CompanionScreen(rememberNavController(), companionVM = CompanionVM())
 }
